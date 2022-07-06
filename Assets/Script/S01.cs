e2b02341b3f4d15f4403e6cdf8bb241278b2c159
user <ufo5220@gmail.com>
2022-07-06 15:49:33 +0800

    圖片顏色及移動

--- Assets/Script/S01.cs
+++ Assets/Script/S01.cs
@@ -1,19 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S01 : MonoBehaviour
 {
-    
+    public Button btn;
+    public Image ima;
+
+    private void Awake()
+    {
+        btn.onClick.AddListener(OnbtnClick);
+
+       
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnbtnClick()
+    {
+        Debug.Log("oooo~");
+
+        ima.color = new Color(0.6f, 0.1f, 0.6f);
+
+        ima.transform .localPosition= new Vector3(20, 30, 0);
+
     }
 }
